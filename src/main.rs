37f2dc2d8895e21772e01ad854871b5556aa49d6
mikael-nilsson-github <35302283+mikael-nilsson-github@users.noreply.github.com>
2022-12-21 10:14:57 -0500

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use eframe;
+
+use navigator::app::switcher::SwitcherApp;
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("egui navigator", native_options, Box::new(|cc| Box::new(SwitcherApp::new(cc))));
+    Ok(())
+}
