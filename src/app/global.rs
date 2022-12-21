14291516bf60c06661c67ee16e267e040a46dc69
mikael-nilsson-github <35302283+mikael-nilsson-github@users.noreply.github.com>
2022-12-21 10:17:08 -0500

    Create global.rs

---
+++ src/app/global.rs
@@ -0,0 +1,17 @@
+pub const COLOR_RED1: egui::Color32 = egui::Color32::from_rgb(255,51,51);
+pub const COLOR_RED2: egui::Color32 = egui::Color32::from_rgb(255,0,0);
+pub const COLOR_RED3: egui::Color32 = egui::Color32::from_rgb(204,0,0);
+
+pub const COLOR_GREEN1: egui::Color32 = egui::Color32::from_rgb(51,255,51);
+pub const COLOR_GREEN2: egui::Color32 = egui::Color32::from_rgb(0,255,0);
+pub const COLOR_GREEN3: egui::Color32 = egui::Color32::from_rgb(0,204,0);
+
+pub const COLOR_BLUE1: egui::Color32 = egui::Color32::from_rgb(51,51,255);
+pub const COLOR_BLUE2: egui::Color32 = egui::Color32::from_rgb(0,0,255);
+pub const COLOR_BLUE3: egui::Color32 = egui::Color32::from_rgb(0,0,204);
+
+pub const COLOR_BLUE_CODE: egui::Color32 = egui::Color32::from_rgb(0,122,204);
+pub const COLOR_GRAY62: egui::Color32 = egui::Color32::from_rgb(62,62,66);
+pub const COLOR_GRAY45: egui::Color32 = egui::Color32::from_rgb(45,45,48);
+pub const COLOR_GRAY37: egui::Color32 = egui::Color32::from_rgb(37,37,38);
+pub const COLOR_GRAY30: egui::Color32 = egui::Color32::from_rgb(30,30,30);
